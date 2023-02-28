1c1a8cee6011d6b231a4f63b00cd9e8105371e1b
NoName <buybackbrain.genetic@gmail.com>
2023-03-01 01:36:24 +0300

    done

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] input = {"asdgh","qwerfsaf","aws","zxc","asdfqa"};
+string[] output;
+int count = 0;
+
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length < 4) count++;
+}
+
+output = new string[count];
+count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length < 4)
+    {
+        output[count] = input[i];
+        count++;
+    }
+}
+
+for (int i = 0; i < output.Length; i++)
+{
+    Console.WriteLine(output[i]);
+}
